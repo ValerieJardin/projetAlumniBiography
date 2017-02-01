cffa639d43f542ba5ba60a00f7f743a62f5fc348
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-01 16:33:12 +0100

    Ajout de div class et de deux class sous titres

--- biography.php
+++ biography.php
@@ -1,2 +1,14 @@
-<?php
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Biographie</title>
+  </head>
+  <body>
+    <div class="biography_personnal">
+      <h2 class="biography_legend">Côté personnel : </h2>
+      <h3 class="biography_label">Mes passions :</h3>
+    </div>
+  </body>
+</html>
