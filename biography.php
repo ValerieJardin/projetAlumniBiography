47fecc0aab3d0c125b5e8862e825bc0aed975308
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-02 15:16:29 +0100

    Autoindent

--- biography.php
+++ biography.php
@@ -1,43 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>Biographie</title>
-  </head>
-  <body>
-    <div class="biography_personnal">
-      <h2 id="biography_legend">Côté personnel </h2>
-      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes passions</h3>
-      <p class="personnal_paragraph">
-<ul>
-  <li>Le MMA (Mix Martial Art)</li>
-  <li>Le trail</li>
-  <li>Le VTT</li>
-  <li>Le cinéma</li>
-</ul>
-      </p>
-      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes liens préférés</h3>
-      <p class="personnal_paragraph">
-<ul>
-  <li><a href="http://formation.novei.fr/"><img class="preferedLinks" src="http://formation.novei.fr/assets/images/logo.jpg" alt="logo E2N"></a></li>
-  <li><a href="https://fr.pinterest.com"><img class="preferedLinks" src="https://lh3.googleusercontent.com/SrULGsyndXJk6Gs4Lmv34sOlMDAroy1bzPPr9X1U5oVdv5TAAJX4JkYPO_wQAdP7TBA=w300" alt="logo pinterest"></a></li>
-  <li><a href="https://secure.php.net"><img class="preferedLinks" src="http://blog.netapsys.fr/wp-content/uploads/2014/11/PHP_LOGO.jpg" alt="logo php"></a></li>
-</ul>
-      </p>
-      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes évènements personnels</h3>
-      <p class="personnal_paragraph">
-Animation d'un atelier "Initiation web developpeur/se" au collège de Noyon.
-      </p>
-      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes actualités personnels</h3>
-      <p class="personnal_paragraph">
-Création d'un site d'entreprise.
-      </p>
-      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Ma famille et plus</h3>
-      <p class="personnal_paragraph">
-        Famille recomposée, nous vivons à six dans un Joyeux Bazar ...
-      </p>
-    </div>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Biographie</title>
+</head>
+<body>
+  <div class="biography_personnal">
+    <h2 id="biography_legend">Côté personnel </h2>
+    <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes passions</h3>
+    <p class="personnal_paragraph">
+      <ul>
+        <li>Le MMA (Mix Martial Art)</li>
+        <li>Le trail</li>
+        <li>Le VTT</li>
+        <li>Le cinéma</li>
+      </ul>
+    </p>
+    <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes liens préférés</h3>
+    <p class="personnal_paragraph">
+      <ul>
+        <li><a href="http://formation.novei.fr/"><img class="preferedLinks" src="http://formation.novei.fr/assets/images/logo.jpg" alt="logo E2N"></a></li>
+        <li><a href="https://fr.pinterest.com"><img class="preferedLinks" src="https://lh3.googleusercontent.com/SrULGsyndXJk6Gs4Lmv34sOlMDAroy1bzPPr9X1U5oVdv5TAAJX4JkYPO_wQAdP7TBA=w300" alt="logo pinterest"></a></li>
+        <li><a href="https://secure.php.net"><img class="preferedLinks" src="http://blog.netapsys.fr/wp-content/uploads/2014/11/PHP_LOGO.jpg" alt="logo php"></a></li>
+      </ul>
+    </p>
+    <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes évènements personnels</h3>
+    <p class="personnal_paragraph">Animation d'un atelier "Initiation web developpeur/se" au collège de Noyon.</p>
+    <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes actualités personnels</h3>
+    <p class="personnal_paragraph">Création d'un site d'entreprise.</p>
+    <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Ma famille et plus</h3>
+    <p class="personnal_paragraph">Famille recomposée, nous vivons à six dans un Joyeux Bazar ...</p>
+  </div>
+</body>
 </html>
