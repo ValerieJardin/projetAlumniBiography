a6249661635f07fa4d30d16f565c045f3260d8bd
JonathanMoroy <jonathan.moroy@gmail.com>
2017-02-02 13:36:16 +0100

    Ecriture, photo, positionnement et style ajouter.

--- biography.php
+++ biography.php
@@ -9,7 +9,13 @@
   <div id="biography_identity">
     <h1 id="biography_title">Biographie de Jonathan</h1>
     <img id="biography_photo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Benzaie_-_Animasia_2014_-_P1940753.jpg/220px-Benzaie_-_Animasia_2014_-_P1940753.jpg" alt="Photo de Jonathan Moroy">
-    <p class="biography_identityInformation">Prénom et Nom: Jonathan Moroy</p>
+    <div id="TEST">
+      <img id="biography_identityLogo" src="http://simplon.co/wp-content/uploads/2016/03/logo_e2n1-e1457183164140.jpg" alt="Ecole E2N">
+      <p class="biography_identityInformationP"><strong>Prénom et Nom:</strong> Jonathan Moroy</p>
+      <p class="biography_identityInformationP"><strong>Date de naissance:</strong> 16/07/1990 (26ans)</p>
+      <p class="biography_identityInformationP"><strong>Actulités Pro:</strong> Apprenant Promo-2</p>
+      <p class="biography_identityInformationP"><strong>@Mail:</strong> jonathan.moroy@gmail.com</p>
+    </div>
   </div>
 </body>
 </html>
