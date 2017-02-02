8b67d76191a595b92de831cc1fc8588b741ac3b7
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-02 09:04:28 +0100

    Ajout de class legend et label

--- biography.php
+++ biography.php
@@ -7,8 +7,13 @@
   </head>
   <body>
     <div class="biography_personnal">
-      <h2 class="biography_legend">Côté personnel : </h2>
+      <h2 id="biography_legend">Côté personnel : </h2>
       <h3 class="biography_label">Mes passions :</h3>
+      <h3 class="biography_label">Mes liens préférés :</h3>
+      <h3 class="biography_label">Ma famille :</h3>
+      <h3 class="biography_label">Mes évènements personnels :</h3>
+      <h3 class="biography_label">Mes actualités personnels :</h3>
+      <h3 class="biography_label">Ma famille et plus :</h3>
     </div>
   </body>
 </html>
