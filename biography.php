1a50aab16ce1d10a10d12f4f4b96e2358a4f609e
Eric.A <arnhemeric60@gmail.com>
2017-02-02 14:02:10 +0100

    modif pour github

--- biography.php
+++ biography.php
@@ -1,2 +1,25 @@
-<?php
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Biographie</title>
+</head>
+<body>
+  <div class="biography_postformation">
+    <h2 id="biography_postMainTitle">Parcours post-formation</h2>
+    <h3 class="biography_postTitle"><i class="material-icons">insert_drive_file</i>Activités Professionelles</h3>
+    <p class="biography_postParagraph">Actuellement: Développeur Back-End chez Google</p>
+    <h3 class="biography_postTitle"><i class="material-icons">insert_drive_file</i>Compétences</h3>
+    <ul id="biography_postSkillsList">
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>PHP</li>
+      <li>...</li>
+    </ul>
+    <h3 class="biography_postTitle"><i class="material-icons">insert_drive_file</i>Notes</h3>
+    <p class="biography_postParagraph">Espace libre pour que la personne puisse ajouter des notes sur sa page (anecdotes,..)</p>
+  </div>
+</body>
+</html>
