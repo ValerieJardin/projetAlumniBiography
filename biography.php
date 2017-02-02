495eb96141ad14f73974c94a7d56748e94f1113b
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-02 11:24:42 +0100

    Ajout d'icones.

--- biography.php
+++ biography.php
@@ -3,17 +3,19 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
+    <script src="https://use.fontawesome.com/d9663e2644.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Biographie</title>
   </head>
   <body>
     <div class="biography_personnal">
-      <h2 id="biography_legend">Côté personnel : </h2>
-      <h3 class="biography_label">Mes passions :</h3>
-      <h3 class="biography_label">Mes liens préférés :</h3>
-      <h3 class="biography_label">Ma famille :</h3>
-      <h3 class="biography_label">Mes évènements personnels :</h3>
-      <h3 class="biography_label">Mes actualités personnels :</h3>
-      <h3 class="biography_label">Ma famille et plus :</h3>
+      <h2 id="biography_legend">Côté personnel </h2>
+      <h3 class="biography_label"><i class="fa fa-file icoBillet fa-2px">Mes passions</h3>
+      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes liens préférés</h3>
+      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Ma famille</h3>
+      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes évènements personnels</h3>
+      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes actualités personnels</h3>
+      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Ma famille et plus</h3>
     </div>
   </body>
 </html>
