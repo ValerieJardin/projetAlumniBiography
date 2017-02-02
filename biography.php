53c2cd7c0e06c8d5f2c3bf0d434df5506430fba1
JonathanMoroy <jonathan.moroy@gmail.com>
2017-02-02 10:00:48 +0100

    Initialisation de la branch identity

--- biography.php
+++ biography.php
@@ -1,2 +1,15 @@
-<?php
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link type="text/css" rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <title>Jonathan Moroy</title>
+</head>
+<body>
+  <div id="biography_identity">
+    <h1 id="biography_title">Biographie de Jonathan</h1>
+    <img id="biography_photo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Benzaie_-_Animasia_2014_-_P1940753.jpg/220px-Benzaie_-_Animasia_2014_-_P1940753.jpg" alt="Photo de Jonathan Moroy">
+    <p class="biography_identityInformation">Prénom et Nom: Jonathan Moroy</p>
+  </div>
+</body>
+</html>
