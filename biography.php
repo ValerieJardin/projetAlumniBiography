3eaaeaa5ed403b96911b4477520825a9ec895282
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-02 11:25:50 +0100

    Changement sur les icones

--- biography.php
+++ biography.php
@@ -3,14 +3,13 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
-    <script src="https://use.fontawesome.com/d9663e2644.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Biographie</title>
   </head>
   <body>
     <div class="biography_personnal">
       <h2 id="biography_legend">Côté personnel </h2>
-      <h3 class="biography_label"><i class="fa fa-file icoBillet fa-2px">Mes passions</h3>
+      <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes passions</h3>
       <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes liens préférés</h3>
       <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Ma famille</h3>
       <h3 class="biography_label"><i class="material-icons">insert_drive_file</i>Mes évènements personnels</h3>
