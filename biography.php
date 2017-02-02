befdbfa7debcc79ec72f319290603cf9916a7822
christophe@christophe-e2n <grarechristophe1@gmail.com>
2017-02-02 14:06:13 +0100

    retouche de branche

--- biography.php
+++ biography.php
@@ -1,2 +1,28 @@
-<?php
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://use.fontawesome.com/d9663e2644.js"></script>
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>christophe grare</title>
+  </head>
+  <body>
+    <body>
+  <h2 id="biography_career_preTraining">Parcours Pre-formation</h2>
+
+    <h3 class="biography_preTraining"><i class="material-icons">insert_drive_file</i>Parcours</h3>
+    <p>
+    <li>diplome bep cap action commercial et comptable.</li>
+    <li>j'ai pratiqué différent metiers qui avai un point commun le physique.</li>
+    </p>
+    <h3 class="biography_preTraining"><i class="material-icons">insert_drive_file</i>Compétences</h3>
+  <p>
+  <li>Mes compétences on evolués au files des metiers occupés, l'adaptation est ma principale.</li>
+  <li>L'envie de comprendre.</li>
+  </p>
+  <h3 class="biography_preTraining"><i class="material-icons">insert_drive_file</i>Texte libre</h3>
+  <p>Un parcour de vie professionel, ou j'ai été militaire, carriste, monteur de ventilation, agent de fabriquation, monteur de meuble.</p>
+  </div>
+</body>
+</html>
