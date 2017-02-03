884acb560acfd2b7cfc721b67e017e15e2beb2a0
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-03 09:53:10 +0100

    Ajout de flex dans CSS

--- biography.php
+++ biography.php
@@ -7,9 +7,9 @@
   <title>Biographie</title>
 </head>
 <body>
+  <h1 id="biography_title">Biographie de Jonathan</h1>
   <div id="biography_identity">
-    <h1 id="biography_title">Biographie de Jonathan</h1>
-    <img id="biography_photo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Benzaie_-_Animasia_2014_-_P1940753.jpg/220px-Benzaie_-_Animasia_2014_-_P1940753.jpg" alt="Photo de Jonathan Moroy">
+    <div><img id="biography_photo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Benzaie_-_Animasia_2014_-_P1940753.jpg/220px-Benzaie_-_Animasia_2014_-_P1940753.jpg" alt="Photo de Jonathan Moroy"></div>
     <div id="biography_identityLogoInformation">
       <img id="biography_identityLogo" src="http://simplon.co/wp-content/uploads/2016/03/logo_e2n1-e1457183164140.jpg" alt="Ecole E2N">
       <p class="biography_identityInformationP"><strong>Prénom et Nom:</strong> Jonathan Moroy</p>
